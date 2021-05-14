488ae96d8a3ff2c8452d67fe25b23be611b0f80c
mukku69 <programing0803@gmail.com>
2021-05-14 09:58:28 +0000

    開発準備完了

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 gem 'pry-byebug'
 gem 'pry-rails'
+gem 'enum_help' 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
+  devise_for :customers
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update]
+    
+    resources :genres, only: [:index, :create, :edit, :update]
+    
+    resources :items, only: [:index, :show, :new, :create, :edit, :update]
+    
+    resources :orders, only: [:show, :update] do
+      resources :order_items, only: [:update]
+    end
+    
+  end
+  
+  scope module: :public do
+    root to: 'homes#top'
+    get "about" => "homes#about"
+    
+    resources :items, only: [:index, :show]
+    
+    resources :cart_items, only: [:index, :create, :update ,:destroy]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    
+    resources :oreders, only: [:index, :show, :create, :new] do
+      collection do
+        post :confirm
+        get :complete
+      end
+    end
+    
+    resources :addresses, only: [:index, :create, :destroy, :edit, :update]
+    
+    resources :customers, only: [:show, :edit, :update] do
+      member do
+        get :quit_confirm
+        patch :quit
+      end
+    end
+    
+  end
 end
