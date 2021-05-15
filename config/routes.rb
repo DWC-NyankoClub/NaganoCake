23af110a98f966fa74f1b83cdf7822407bae4129
mukku69 <78787186+mukku69@users.noreply.github.com>
2021-05-15 11:38:21 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :customers
-  devise_for :admin
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     root to: 'homes#top'
