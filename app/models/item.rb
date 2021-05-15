17a03a6f255fc76e93c29b2b27428707b1772949
TajimaRyosuke <east.helman56@gmail.com>
2021-05-15 10:00:04 +0000

    admin商品機能追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,17 @@ class Item < ApplicationRecord
   validates :price, presence: true
 
   attachment :image
+
+  def is_active_to_string
+    if is_active == true
+      "販売中"
+    else
+      "販売停止中"
+    end
+  end
+
+  def tax_included_price
+    tax_rate = 1.08
+    price * tax_rate
+  end
 end
