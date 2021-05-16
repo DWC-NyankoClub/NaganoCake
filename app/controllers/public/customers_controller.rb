196c988707a73d6f6f2b0a8e28e47c731089000e
mukku69 <programing0803@gmail.com>
2021-05-16 02:42:30 +0000

    Bootstrap実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,4 +4,26 @@ class Public::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+       flash[:success] = "登録情報を変更しました。"
+       redirect_to customer_path
+    else
+        render 'edit'
+    end
+  end
+
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postcode, :address, :phone_number, :is_deleted)
+  end
+
 end
