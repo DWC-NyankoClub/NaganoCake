523e7b90ada7103258d48aa816f3729d83b65018
TajimaRyosuke <east.helman56@gmail.com>
2021-05-17 10:46:39 +0000

    注文情報入力機能追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,19 @@
 class Public::OrdersController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+    @order = Order.new
+    @addresses = Address.all
+  end
+
+  def confirm
+  end
+
+  def complete
+  end
+
 end
