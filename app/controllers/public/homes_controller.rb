8d967fdd7849dbc6da2021072b58932a2840b547
docty89 <docty89@gmail.com>
2021-05-18 05:57:55 +0000

    js

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,9 @@
 class Public::HomesController < ApplicationController
   
   def top
+    # limit=>指定した件数取得,order=>並び替え,desc=>降順
+    @items = Item.limit(4).order(created_at: :desc)
+    @genres = Genre.all
   end
     
   def about
