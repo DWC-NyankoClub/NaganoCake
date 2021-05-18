6aac805e72bc9590ee47db7cf0b53871361ab95a
TajimaRyosuke <east.helman56@gmail.com>
2021-05-18 05:56:21 +0000

    商品注文確定機能追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,8 @@
 class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
+
+  def subtotal
+    self.item.tax_included_price * self.quantity
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
-  
+
   enum pay_type: {クレジットカード:0, 銀行振込:1}
   enum buy_status: {入金待ち:0, 入金確認:1,  製作中:2, 発送準備中:3, 発送済み:4}
+
+  def shipping_cost
+    800
+  end
+
+  def cal_price(customer)
+    shipping_cost + customer.cart_items.sum(&:subtotal)
+  end
+
 end
