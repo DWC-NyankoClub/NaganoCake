bad3e1f474adb553fb86764cbc193ceee60f4b47
docty89 <docty89@gmail.com>
2021-05-18 09:07:22 +0000

    注文詳細履歴

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,13 +2,16 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @orders = Order.page(params[:page]).per(10)
-    @orders = Order.all
+    path = Rails.application.routes.recognize_path(request.referer)
+
+    if path[:controller]=="admin/customers" && path[:action] == "show"
+      @orders = Order.where(customer_id: path[:id])
+    else
+      @orders = Order.all
+    end
   end
 
-  
-  def about
-  end
+
   
 
 end
