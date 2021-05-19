d2224a8654c2232d7b65ca0efd0343887939774c
docty89 <docty89@gmail.com>
2021-05-19 05:25:40 +0000

    注文詳細ステータス実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,26 +3,24 @@ class Admin::OrdersController < ApplicationController
     
   def show
     @order = Order.find(params[:id])
-    @order_items = OrderItem.all
+    @order_items = @order.order_items
   end
   
   def update
-    @order = Order.find(params[:id])
-    if @order.update(order_params) && @order.confirm_deposit?
-            @order.order_items.update_all(make_status: 1)
-      redirect_to admin_order_path(@order)
-    else
-      render :show
-    end    
+    order = Order.find(params[:id])
+    order.update(order_params)
+    order.order_item_status_auto_update
+    redirect_to admin_order_path(params[:id])
   end
+  
   
   private
 
-  def order_item_params
-    params.require(:order_item).permit(:quantity, :make_status, :price)
-  end
+
   def order_params
     params.require(:order).permit(:buy_status)
-  end  
+  end
+
+
   
 end
